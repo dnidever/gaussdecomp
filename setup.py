1b04e89b4beb58d344ca6d39c2a87ad179792f18
David Nidever <dnidever@lsst.org>
2022-03-24 20:22:31 -0600

    bumped version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gaussdecomp',
-      version='1.0.0',
+      version='1.0.1',
       description='Gaussian Decomposition',
       author='David Nidever',
       author_email='dnidever@montana.edu',
