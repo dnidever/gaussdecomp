d4ae1cb4874c011fb294bb9182003a2ed77dad01
David Nidever <dnidever@lsst.org>
2022-06-23 13:27:31 -0600

    fixed how dethtresh is used and modified snr/ht thresholds

--- gaussdecomp/fitter.py
+++ gaussdecomp/fitter.py
@@ -7,7 +7,7 @@ from . import utils
 from dlnpyutils import utils as dln
 
 def gaussfitter(spectrum,initpar=None,noplot=True,silent=False,
-                vmax=None,vmin=None,detsnrthresh=0,dethtthresh=2.0,
+                vmax=None,vmin=None,detsnrthresh=5,dethtthresh=1.0,
                 debug=False,noremovesmall=False):
                 
     """
@@ -29,10 +29,10 @@ def gaussfitter(spectrum,initpar=None,noplot=True,silent=False,
     silent : boolean, optional
        Don't print anything 
     detsnrthresh : float, optional
-       Detection threshold on S/N.  Default is 0.
+       Detection threshold on S/N.  Default is 5.
     dethtthresh : float, optional
        Detection threshold on height in units of noise.
-         Default is 2.
+         Default is 1.
     debug : boolean, optional
        Diagnostic printing and plotting.
     noremovesmall : boolean, optional
@@ -179,21 +179,22 @@ def gaussfitter(spectrum,initpar=None,noplot=True,silent=False,
                 smresid = smresid100 
          
             # Getting maxima 
-            maxarr = utils.gpeak1(smresid,np.maximum(5*noise,0.5*maxy))
-            if len(maxarr)==0:
-                maxarr = utils.gpeak1(smresid,np.maximum(5*noise,0.1*maxy))
-            if len(maxarr)==0 and dethtthresh<=5:
-                maxarr = utils.gpeak1(smresid,5*noise)
-            if len(maxarr)==0 and dethtthresh<=3:
-                maxarr = utils.gpeak1(smresid,3*noise)
-            if len(maxarr)==0 and dethtthresh<=2:
-                maxarr = utils.gpeak1(smresid,2*noise)
-            if len(maxarr)==0 and dethtthresh<=1:
-                maxarr = utils.gpeak1(smresid,noise)
-            if len(maxarr)==0 and dethtthresh<=0.5:
-                maxarr = utils.gpeak1(smresid,0.5*noise)
-            if len(maxarr)==0 and dethtthresh<0.5:
-                maxarr = utils.gpeak1(smresid,dethtthresh*noise)                
+            maxarr = utils.gpeak1(smresid,dethtthresh*noise)
+            #maxarr = utils.gpeak1(smresid,np.maximum(5*noise,0.5*maxy))            
+            #if len(maxarr)==0:
+            #    maxarr = utils.gpeak1(smresid,np.maximum(5*noise,0.1*maxy))
+            #if len(maxarr)==0 and dethtthresh<=5:
+            #    maxarr = utils.gpeak1(smresid,5*noise)
+            #if len(maxarr)==0 and dethtthresh<=3:
+            #    maxarr = utils.gpeak1(smresid,3*noise)
+            #if len(maxarr)==0 and dethtthresh<=2:
+            #    maxarr = utils.gpeak1(smresid,2*noise)
+            #if len(maxarr)==0 and dethtthresh<=1:
+            #    maxarr = utils.gpeak1(smresid,noise)
+            #if len(maxarr)==0 and dethtthresh<=0.5:
+            #    maxarr = utils.gpeak1(smresid,0.5*noise)
+            #if len(maxarr)==0 and dethtthresh<0.5:
+            #    maxarr = utils.gpeak1(smresid,dethtthresh*noise)                
             ngd = len(maxarr) 
             
             # If there are any peaks check them out 
